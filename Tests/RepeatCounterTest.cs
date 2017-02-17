5c7ad46c1ee9c8fbfd3347b6c13f5ea9a4a665a7
Renee <meiqianye@gmail.com>
2017-02-17 08:47:47 -0800

    add test for the second spec

--- Tests/RepeatCounterTest.cs
+++ Tests/RepeatCounterTest.cs
@@ -6,11 +6,18 @@ namespace RepeatCounterProjectTest
 {
   public class RepeatCounterTest
   {
-    [Fact]
+    [Fact]//Spec 1
     public void RepeatCounterTest_ForNoMatchesFound_0()
     {
       RepeatCounter testRepeatCounter = new RepeatCounter();
       Assert.Equal(0, testRepeatCounter.CountRepeats("Hello", "Good Morning"));
+    }
+
+    [Fact]//Spec 2
+    public void RepeatCounterTest_ForWordAndStringExactlySame_1()
+    {
+      RepeatCounter testRepeatCounter = new RepeatCounter();
+      Assert.Equal(1, testRepeatCounter.CountRepeats("Hello", "Hello"));
     }
   }
 }
